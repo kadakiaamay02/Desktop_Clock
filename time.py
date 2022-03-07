24094fa1e4b7d64e99e81e500a790b2641044af8
kadakiaamay02 <72008713+kadakiaamay02@users.noreply.github.com>
2022-03-06 22:06:03 -0600

    Create time.py

---
+++ time.py
@@ -0,0 +1,24 @@
+# importing tkinter module used for gui
+from tkinter import *
+from tkinter.ttk import *
+
+# strftime  will import current time from your device
+from time import strftime
+
+# setting up a tkinter window
+root = Tk()
+root.title('Clock')
+
+def time():
+	string = strftime('%H:%M:%S %p')
+	lbl.config(text = string)
+	lbl.after(1000, time)
+
+lbl = Label(root, font = ('Open sans', 200, 'bold'),
+			background = 'black',
+			foreground = 'white')
+
+lbl.pack(anchor = 'center')
+time()
+
+mainloop()
